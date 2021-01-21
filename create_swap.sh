cf65de7be2382d6c382dd5d9d51a887e882a3700
Igor <55797269+igorlachman@users.noreply.github.com>
2021-01-21 14:34:49 +0200

    Update create_swap.sh
    -- check if user root
    -- echo disk space and swap_file in MB
    -- adding vm.swappiness=5
    -- backup fstab and sysctl.conf

--- create_swap.sh
+++ create_swap.sh
@@ -3,65 +3,93 @@ ROOT_SWAP_DIR='/swap/'
 SWAP_FILE_NAME='swap.img'
 SWAP_STATUS=`free -m | grep -i swap | awk '{print $2}'`
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 0
+fi
+
 if [[ $SWAP_STATUS != '0' ]]; then
-echo "SWAP EXIST. USAGE - "$SWAP_STATUS
+echo "SWAP exist. [Usage SWAP] - "$SWAP_STATUS
 exit 1
 fi
 
-if [[ $SWAP_STATUS = '0' ]]; then
-echo "SWAP NOT EXIST. USAGE - "$SWAP_STATUS" ADD SWAP ? YES/NO"
-read USER_ANSWER
-fi
+while [ 1 ]
+do
+    if [[ $SWAP_STATUS = '0' ]]; then
+        echo "SWAP not exist. [Usage SWAP] - "$SWAP_STATUS" Add SWAP ? YES/NO"
+        read USER_ANSWER
+    fi
 
-if [[ $USER_ANSWER = 'NO' ]]; then
-echo "Nothing to do."
-elif [[ $USER_ANSWER = 'YES' ]]; then
-DO_STATUS='YES'
-FREE_DISK=`df -m /|awk '{print $4}'|grep -v 'Avail'`
-echo "Input filesize of swapfile in MB (example - 1Gb type 1024)"
-read USER_ANSWER_SWAP_FILESIZE
-else
-echo "Type only YES or NO"
-fi
+    if [[ $USER_ANSWER = 'NO' ]]; then
+        echo "Nothing to do."
+        exit 0
+    elif [[ $USER_ANSWER = 'YES' ]]; then
+        DO_STATUS='YES'
+        break
+    else
+        echo "Type only YES or NO"
+    fi
+done
 
-if (echo "$USER_ANSWER_SWAP_FILESIZE" | grep -E -q "^?[0-9]+$"); then
-USER_ANSWER_SWAP_FILESIZE_STATUS='NUMBER'
-else
-USER_ANSWER_SWAP_FILESIZE_STATUS='NOT_NUMBER'
-fi
-
-if [[ $USER_ANSWER_SWAP_FILESIZE_STATUS = 'NOT_NUMBER' ]]; then
-echo "SWAP is int. Not string. Can't create SWAP with your choice "$USER_ANSWER_SWAP_FILESIZE
-exit 1
-fi
-
-if [[ $USER_ANSWER_SWAP_FILESIZE -ge $FREE_DISK ]]; then
-echo "The filesize of SWAP is too big. Not enough disk spase"
-fi
-
+while [ 1 ]
+do
 if [[ $DO_STATUS = 'YES' ]]; then
-FREE_SPASE_LEFT=$((FREE_DISK-USER_ANSWER_SWAP_FILESIZE))
-echo "Free disk space will be "$FREE_SPASE_LEFT" MB"
-echo "Continue ? YES/NO"
-read USER_ANSWER_FREE_LEFT
-fi
 
-if [[ $USER_ANSWER_FREE_LEFT = 'NO' ]]; then
-echo "Nothing to do."
-elif [[ $USER_ANSWER_FREE_LEFT = 'YES' ]]; then
-STATUS_CREATE_SWAP='NEED_TO_CREATE'
-else
-echo "Type only YES or NO"
+    FREE_DISK=`df -m /|awk '{print $4}'|grep -v 'Avail'`
+    echo "[$FREE_DISK MB free] Input filesize of swapfile in MB (example. If need add 1Gb of SWAP - type 1024)"
+    read USER_ANSWER_SWAP_FILESIZE
+
+    if (echo "$USER_ANSWER_SWAP_FILESIZE" | grep -E -q "^?[0-9]+$"); then
+        USER_ANSWER_SWAP_FILESIZE_STATUS='NUMBER'
+        break
+    else
+        echo "SWAP is int. Not string. Can't create SWAP with your choice "$USER_ANSWER_SWAP_FILESIZE
+        continue
+    fi
+
+    if [[ $USER_ANSWER_SWAP_FILESIZE -ge $FREE_DISK ]]; then
+        echo "The filesize of SWAP is too big. Not enough disk spase"
+        exit 1
+    fi
+
 fi
+done
+
+while [ 1 ]
+do
+
+    if [[ $DO_STATUS = 'YES' ]]; then
+        FREE_SPASE_LEFT=$((FREE_DISK-USER_ANSWER_SWAP_FILESIZE))
+        echo "Free disk space will be "$FREE_SPASE_LEFT" MB"
+        echo "Continue ? YES/NO"
+        read USER_ANSWER_FREE_LEFT
+    fi
+
+    if [[ $USER_ANSWER_FREE_LEFT = 'NO' ]]; then
+        echo "Nothing to do."
+        exit 0
+    elif [[ $USER_ANSWER_FREE_LEFT = 'YES' ]]; then
+        STATUS_CREATE_SWAP='NEED_TO_CREATE'
+        break
+    else
+        echo "Type only YES or NO"
+    fi
+
+done
 
 if [[ $STATUS_CREATE_SWAP = 'NEED_TO_CREATE' ]]; then
-mkdir $ROOT_SWAP_DIR
-`fallocate -l $USER_ANSWER_SWAP_FILESIZE"MB" $ROOT_SWAP_DIR$SWAP_FILE_NAME`
-chmod 0600 $ROOT_SWAP_DIR$SWAP_FILE_NAME
-mkswap $ROOT_SWAP_DIR$SWAP_FILE_NAME
-swapon $ROOT_SWAP_DIR$SWAP_FILE_NAME
-echo "$ROOT_SWAP_DIR$SWAP_FILE_NAME swap swap sw 0 0" >> /etc/fstab
-echo "[CREATING FILE] - "$ROOT_SWAP_DIR$SWAP_FILE_NAME" [FILESIZE] = "$USER_ANSWER_SWAP_FILESIZE"MB"
-echo "DONE"
-free -m
+    echo "[CREATING FILE] - "$ROOT_SWAP_DIR$SWAP_FILE_NAME" [FILESIZE] = "$USER_ANSWER_SWAP_FILESIZE"MB"
+    mkdir $ROOT_SWAP_DIR
+    `fallocate -l $USER_ANSWER_SWAP_FILESIZE"MB" $ROOT_SWAP_DIR$SWAP_FILE_NAME`
+    chmod 0600 $ROOT_SWAP_DIR$SWAP_FILE_NAME
+    mkswap $ROOT_SWAP_DIR$SWAP_FILE_NAME
+    swapon $ROOT_SWAP_DIR$SWAP_FILE_NAME
+    timestamp=$(date +%s)
+    cp -rp /etc/fstab /etc/fstab$timestamp
+    echo "$ROOT_SWAP_DIR$SWAP_FILE_NAME swap swap sw 0 0" >> /etc/fstab
+    cp -rp /etc/sysctl.conf /etc/sysctl.conf$timestamp
+    echo 'vm.swappiness=5' >> /etc/sysctl.conf
+    sysctl -p
+    echo "[DONE]"
+    free -m
 fi
